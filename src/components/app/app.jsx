adc34e7dea6c299d581dc6d29333d38e81351a45
Ildar <misterildar@gmail.com>
2023-06-19 20:58:29 +0300

    start

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,24 @@
 import styles from './app.module.css';
 import { data } from '../../utils/data';
-import { AppHeader } from '../app-header/AppHeader';
+// import { AppHeader } from '../app-header/app-header';
 
 function App() {
-  return (
-    <div className={styles.app}>
-      <pre
-        style={{
-          margin: 'auto',
-          fontSize: '1.5rem',
-        }}
-      >
-        Измените src/components/app/app.jsx и сохраните для обновления.
-      </pre>
-    </div>
-  );
+  return <div>{/* <AppHeader /> */}</div>;
 }
+
+export default App;
+
+// function App() {
+//   return (
+//     <div className={styles.app}>
+//       <pre
+//         style={{
+//           margin: 'auto',
+//           fontSize: '1.5rem',
+//         }}
+//       >
+//         Измените src/components/app/app.jsx и сохраните для обновления.
+//       </pre>
+//     </div>
+//   );
+// }
