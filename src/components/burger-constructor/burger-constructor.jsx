cb14174c418a259b76113654266cbbf9bca4a8b4
Ildar <misterildar@gmail.com>
2023-07-10 20:43:01 +0300

    10.07

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,6 +1,8 @@
 import React, { useMemo } from 'react';
 import styles from './burger-constructor.module.css';
-import { BurgerConstructorList } from '../burger-constructor-list/burger-constructor-list';
+// import { BurgerConstructorList } from '../burger-constructor-list/burger-constructor-list';
+import { CurrencyIcon, DragIcon, DeleteIcon, ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
+
 import { CreateOrder } from '../create-order/create-order';
 import PropTypes from 'prop-types';
 import { ingredientPropType } from '../../utils/prop-types';
@@ -8,12 +10,25 @@ import { ingredientPropType } from '../../utils/prop-types';
 export const BurgerConstructor = ({ ingredients }) => {
   let sum = useMemo(() => ingredients.reduce((acc, price) => acc + price.price, 0), [ingredients]);
 
+  const saucesAndMains = useMemo(() => ingredients.filter((el) => el.type === 'sauce' || el.type === 'main'), [ingredients]);
+
   return (
     <section className={styles.section}>
-      <div className={`${styles.box} custom-scroll mt-15 pb-10`}>
-        {ingredients.map((el) => (
-          <BurgerConstructorList lists={el} key={el._id} />
-        ))}
+      <div className={styles.box}>
+        <ConstructorElement type="top" isLocked={true} text="Краторная булка N-200i (верх)" price={200} thumbnail={'img'} />
+
+        <div className={`${styles.container} custom-scroll`}>
+          {saucesAndMains.map((el) => {
+            return (
+              <div key={el._id} className={styles.item}>
+                <DragIcon type="primary" />
+                <ConstructorElement text={el.name} price={el.price} thumbnail={el.image} />
+              </div>
+            );
+          })}
+        </div>
+
+        <ConstructorElement type="bottom" isLocked={true} text="Краторная булка N-200i (низ)" price={200} thumbnail={'img'} />
       </div>
       <CreateOrder sum={sum} />
     </section>
--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -3,6 +3,7 @@ import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 
 export const Navigation = () => {
   const [current, setCurrent] = React.useState('one');
+
   return (
     <div style={{ display: 'flex' }}>
       <Tab value="one" active={current === 'one'} onClick={setCurrent}>
